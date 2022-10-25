0ae69dd06f7f6221f40a30985f3621c1e9b360eb
Joao Moreira <jgmoreira1809@gmail.com>
2022-10-25 18:02:40 -0300

    Models OK

---
+++ src/models/User.model.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true,
+  });
+  return User;
+}
